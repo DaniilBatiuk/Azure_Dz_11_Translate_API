538c62ccd907dd26ee8096a45b7dec5add064aed
DaniilBatiuk <sportak50@gmail.com>
2023-05-27 10:49:13 +0300

    Добавьте файлы проекта.

---
+++ Azure_Dz_11_Translate_API/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using System.Text;
+using System.Net.Http.Headers;
+using Azure_Dz_11_Translate_API;
+
+string endpoint = "https://api.cognitive.microsofttranslator.com/";
+string bearerToken = "bearerToken";
+string url = "translate?api-version=3.0&to=en&to=de&from=uk&to=pl";
+string requestUrl = endpoint + url;
+using (HttpClient client = new HttpClient())
+using (HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, requestUrl))
+{
+    requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+
+    object[] textForTranslate = new[] {
+        new {Text = "Мене цікавить вивчення і використання хмарних сервісів для розробки програмного забезпечення" },
+        new {Text = "Доброго вечора, ми з України"}
+    };
+
+    string body = JsonSerializer.Serialize(textForTranslate);
+
+    requestMessage.Content = new StringContent(body, encoding: Encoding.UTF8, "application/json");
+    HttpResponseMessage message = await client.SendAsync(requestMessage);
+
+    string translation = await message.Content.ReadAsStringAsync();
+
+    List<TranslationResult>? translationResults = JsonSerializer.Deserialize<List<TranslationResult>>(translation);
+
+    foreach (TranslationResult result in translationResults!)
+    {
+        Console.WriteLine("=>");
+        foreach (Translation transl in result.Translations)
+        {
+            Console.WriteLine($"To {transl.To}: {transl.Text}");
+        }
+    }
+    Console.WriteLine("====================================\n");
+}
---
+++ Azure_Dz_11_Translate_API/TranslationResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure_Dz_11_Translate_API
+{
+    public class Translation
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+        [JsonPropertyName("to")]
+        public string To { get; set; }
+    }
+
+    public class TranslationResult
+    {
+        [JsonPropertyName("translations")]
+        public IList<Translation> Translations { get; set; }
+    }
+}
